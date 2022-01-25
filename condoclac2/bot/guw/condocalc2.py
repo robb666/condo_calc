78b9484e95b5305cd8bdb42c4359313df15be9d8
Robb <56496611+robb666@users.noreply.github.com>
2022-01-25 20:06:54 +0100

    update

--- condoclac2/bot/guw/condocalc2.py
+++ condoclac2/bot/guw/condocalc2.py
@@ -11,7 +11,10 @@ class Condocalc(webdriver.Chrome):
     def __init__(self, driver_path=webdriver.Chrome, teardown=False):
         self.driver_path = driver_path
         self.teardown = teardown
-        super(Condocalc, self).__init__()
+        options = webdriver.ChromeOptions()
+        # options.add_experimental_option('excludeSwitches', ['enable-logging'])  # win devtools
+        # options.add_argument('--headless')
+        super(Condocalc, self).__init__(options=options)
         self.implicitly_wait(10)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -25,4 +28,11 @@ class Condocalc(webdriver.Chrome):
         self.find_element(By.XPATH, "//input[@id='username' or @id='login']").send_keys(login)
         self.find_element(By.XPATH, "//input[@id='password']").send_keys(passw)
         self.find_element(By.XPATH, "//*[@type='submit' or @type='button']").click()
-        time.sleep(9)
+
+    def calc(self):
+        self.find_element(By.XPATH, "//label[contains(text(), 'Dom')]").click()
+
+    def apk(self):
+        self.find_element(By.XPATH, "//span[@class='fe-radio-btn-label']").click()
+        # self.find_element(By.XPATH, "//span[@class='fe-radio-btn-label']").click()
+        time.sleep(9999)
--- condoclac2/bot/run.py
+++ condoclac2/bot/run.py
@@ -1,7 +1,6 @@
 from guw.condocalc2 import Condocalc
 import guw.constants as const
 import concurrent.futures
-import time
 
 
 def gen():
@@ -9,28 +8,29 @@ def gen():
         bot.land_first_page(const.login['generali_url'])
         bot.login(const.login['generali_login'],
                   const.login['generali_password'])
+        bot.calc()
+        bot.apk()
+
+# def war():
+#     with Condocalc() as bot:
+#         bot.land_first_page(const.login['warta_url'])
+#         bot.login(const.login['warta_login'],
+#                   const.login['warta_password'])
+#
+#
+# def wie():
+#     with Condocalc() as bot:
+#         bot.land_first_page(const.login['wiener_url'])
+#         bot.login(const.login['wiener_login'],
+#                   const.login['wiener_password'])
+#
+#
+# calcs = [gen, war, wie]
+# with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+#     for calc in calcs:
+#         executor.submit(calc)
 
 
-def war():
-    with Condocalc() as bot:
-        bot.land_first_page(const.login['warta_url'])
-        bot.login(const.login['warta_login'],
-                  const.login['warta_password'])
 
 
-def wie():
-    with Condocalc() as bot:
-        bot.land_first_page(const.login['wiener_url'])
-        bot.login(const.login['wiener_login'],
-                  const.login['wiener_password'])
-
-
-calcs = [gen, war, wie]
-with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    for calc in calcs:
-        executor.submit(calc)
-
-
-
-
-
+gen()
