38f2e8e8786d2ba1c2fe1951a4a40929b24353c0
Robb <56496611+robb666@users.noreply.github.com>
2022-04-02 15:44:31 +0200

    up

--- condoclac2/bot/gww/constants.py
+++ condoclac2/bot/gww/constants.py
@@ -1,24 +1,46 @@
 import pandas as pd
 import os
 
-PATH = 'M:\Agent baza\Login_Hasło.xlsx'
-
-
-login = {}
-df = pd.read_excel(PATH)
-login['generali_url'] = 'https://' + df.iloc[8, 3]
-login['generali_login'] = df.iloc[8, 5]
-login['generali_password'] = df.iloc[8, 6]
-
-login['warta_url'] = 'https://' + df.iloc[42, 3]
-login['warta_login'] = df.iloc[42, 5]
-login['warta_password'] = df.iloc[42, 6]
-
-login['wiener_url'] = 'https://' + df.iloc[52, 3]
-login['wiener_login'] = df.iloc[52, 5]
-login['wiener_password'] = df.iloc[52, 6]
-
-
 WIE_CALC = 'https://wienet.pl/#/household?calculatorRoute=household'
 
 DATA_PATH = os.getcwd() + r'\formularz mieszkaniowy.txt'
+
+JAREK_PATH = r'C:\LiH\HASŁA A.xlsx'
+
+
+login = {}
+if os.path.exists(JAREK_PATH):
+    PATH = JAREK_PATH
+
+    df = pd.read_excel(PATH)
+
+    login['generali_url'] = 'https://portal.generali.pl'
+    login['generali_login'] = df.iloc[0, 1]
+    login['generali_password'] = df.iloc[0, 2]
+
+    login['warta_url'] = 'https://eagent.warta.pl'
+    login['warta_login'] = df.iloc[1, 1]
+    login['warta_password'] = df.iloc[1, 2]
+
+    login['wiener_url'] = 'https://wienet.pl/#/login'
+    login['wiener_login'] = df.iloc[2, 1]
+    login['wiener_password'] = df.iloc[2, 2]
+
+
+else:
+    PATH = r'M:\Agent baza\Login_Hasło.xlsx'
+
+    df = pd.read_excel(PATH)
+
+    login['generali_url'] = 'https://' + df.iloc[8, 3]
+    login['generali_login'] = df.iloc[8, 5]
+    login['generali_password'] = df.iloc[8, 6]
+
+    login['warta_url'] = 'https://' + df.iloc[42, 3]
+    login['warta_login'] = df.iloc[42, 5]
+    login['warta_password'] = df.iloc[42, 6]
+
+    login['wiener_url'] = 'https://' + df.iloc[52, 3]
+    login['wiener_login'] = df.iloc[52, 5]
+    login['wiener_password'] = df.iloc[52, 6]
+
