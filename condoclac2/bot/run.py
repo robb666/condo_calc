7b157f4979724c43a8711d2214443722ef664694
Robb <56496611+robb666@users.noreply.github.com>
2022-07-21 08:58:00 +0200

    Update run.py

--- condoclac2/bot/run.py
+++ condoclac2/bot/run.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     data = customer_data(const.DATA_PATH)
     pprint(data)
 
-    wie(data) ####
+    war(data) ####
     # Przez concurrent.futures mie ma traceback
 
 
